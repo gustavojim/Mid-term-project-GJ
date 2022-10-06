79c1fa609be41ece4fe55adb1e219499e0f05d35
gustavojim <113261314+gustavojim@users.noreply.github.com>
2022-10-06 18:53:33 +0200

    Hecho menú hambuguesa

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+function myFunction() {
+  var x = document.getElementsByClassName("navbar-item-a");
+  for (let i = 0; i < x.length; i++) {
+
+    if (x[i].style.display === "block") {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "block";
+    }
+  }
+}
